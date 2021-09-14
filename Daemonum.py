d2cc40c83bd952d1df7a29655b7ceb275c24b719
RachaelC358 <80548521+RachaelC358@users.noreply.github.com>
2021-09-14 12:36:03 -0500

    Add files via upload

---
+++ Daemonum.py
@@ -0,0 +1,51 @@
+#
+# Quantum Daemonum 
+# A simple game where you play as as a wicked spirit in a battle with
+# Van Helsing who is played by a quantum computer. Van Helsing will
+# attempt to guess your true name in an attempt to gain power over and
+# and defeat you. You can attempt to thwart him with spiritual attacks
+# that can either weaken his ability to guess or hide your name entirely.
+#
+
+
+import qiskit
+from qiskit import ClassicalRegister, QuantumRegister, QuantumCircuit
+from qiskit import IBMQ
+import math
+
+
+def runGame():
+
+
+def action(option):
+    option = option.lower(0);
+
+    switcher = {
+    # thwart
+    't':
+    # 
+    '':
+    #
+    '':
+    }
+
+
+def getDemonName(index):
+    names = {
+        1:'Dusios',
+        2:'Belial',
+        3:'Belphegor', 
+        4:'Andromalius',
+        5:'Astaroth',
+        6:'Beleth',
+        7:'Drekevac',
+        8:'Demiurge',
+        9:'Forneus',
+        10:'Gomory',
+        11:'Haures',
+        12:'Pithius',
+        13:'Mammon',
+        14:'Marchosias',
+        15:'Moloch',
+        16:'Naphula'
+    }
